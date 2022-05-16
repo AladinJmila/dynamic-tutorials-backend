98c3fb584bfd95855f1b8eb29f49c492c5abb908
Alaeddine Jmila <jmila.aladin@gmail.com>
2022-05-16 12:04:56 +0100

    First commit after UI rebuild kick off

--- src/main.js
+++ src/main.js
@@ -1,15 +1,15 @@
-import populateSidebar from './UI/sidebarRender';
-import injectLevelTow from './UI/mediaRender';
-import saveMedia from './media/mediaHandler';
+// import populateSidebar from './UI/sidebarRender';
+// import injectLevelTow from './UI/mediaRender';
+// import saveMedia from './media/mediaHandler';
 import './main.css';
 
-const hideBtn = document.getElementById('hide-sidebar');
-const sidebarEl = document.getElementById('sidebar');
+// const hideBtn = document.getElementById('hide-sidebar');
+// const sidebarEl = document.getElementById('sidebar');
 
-hideBtn.addEventListener('click', () => {
-  sidebarEl.classList.toggle('hide');
-});
+// hideBtn.addEventListener('click', () => {
+//   sidebarEl.classList.toggle('hide');
+// });
 
-populateSidebar();
-injectLevelTow();
-saveMedia();
+// populateSidebar();
+// injectLevelTow();
+// saveMedia();
