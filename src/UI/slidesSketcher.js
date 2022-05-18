7983885a4f2f83b5fa0cb7897a7c7763eb596c06
Alaeddine Jmila <jmila.aladin@gmail.com>
2022-05-18 20:59:14 +0100

    Added drawing a rectangle and added functionality to generate an image with the rec on it

--- src/UI/slidesSketcher.js
+++ src/UI/slidesSketcher.js
@@ -1,11 +1,9 @@
 export default function slidesSketcher() {
-  let isDrawing = false;
-  let x = 0;
-  let y = 0;
-
   const canvasContainer = document.querySelector('.canvas-container');
   const canvas = document.getElementById('canvas');
-  const context = canvas.getContext('2d');
+  const ctx = canvas.getContext('2d');
+  let mousedown = false;
+  const imageObj = new Image();
 
   function resizeCanvas() {
     canvas.setAttribute('height', canvasContainer.clientHeight);
@@ -17,36 +15,72 @@ export default function slidesSketcher() {
   resizeCanvas();
   window.addEventListener('resize', resizeCanvas);
 
-  function drawLine(context, x1, y1, x2, y2) {
-    context.beginPath();
-    context.strokeStyle = 'white';
-    context.lineWidth = 3;
-    context.moveTo(x1, y1);
-    context.lineTo(x2, y2);
-    context.stroke();
-    context.closePath();
+  function scaleToFit(img) {
+    const scale = Math.min(
+      canvas.width / img.width,
+      canvas.height / img.height
+    );
+    const x = canvas.width / 2 - (img.width / 2) * scale;
+    const y = canvas.height / 2 - (img.height / 2) * scale;
+    ctx.drawImage(img, x, y, img.width * scale, img.height * scale);
+  }
+
+  imageObj.src = '/images/sakura.jpg';
+
+  imageObj.onload = function () {
+    scaleToFit(this);
+  };
+
+  const clicks = [];
+
+  function drawRectangle() {
+    ctx.beginPath();
+    ctx.rect(
+      clicks[0].x,
+      clicks[0].y,
+      clicks[1].x - clicks[0].x,
+      clicks[1].y - clicks[0].y
+    );
+    ctx.strokeStyle = 'red';
+    ctx.lineWidth = 3;
+    ctx.stroke();
+  }
+
+  function redraw() {
+    canvas.width = canvas.width;
+    scaleToFit(imageObj);
+
+    drawRectangle();
   }
 
   canvas.addEventListener('mousedown', e => {
-    x = e.offsetX;
-    y = e.offsetY;
-    isDrawing = true;
+    clicks[0] = {
+      x: e.offsetX,
+      y: e.offsetY,
+    };
+    mousedown = true;
   });
 
   canvas.addEventListener('mousemove', e => {
-    if (isDrawing === true) {
-      drawLine(context, x, y, e.offsetX, e.offsetY);
-      x = e.offsetX;
-      y = e.offsetY;
+    if (mousedown) {
+      clicks[1] = {
+        x: e.offsetX,
+        y: e.offsetY,
+      };
+      redraw();
     }
   });
 
-  window.addEventListener('mouseup', e => {
-    if (isDrawing === true) {
-      drawLine(context, x, y, e.offsetX, e.offsetY);
-      x = 0;
-      y = 0;
-      isDrawing = false;
-    }
+  canvas.addEventListener('mouseup', e => {
+    mousedown = false;
+    clicks[1] = {
+      x: e.offsetX,
+      y: e.offsetY,
+    };
+    console.log(canvas.toDataURL('image/jpeg', 1.0));
+  });
+
+  canvas.addEventListener('mouseleave', () => {
+    mousedown = false;
   });
 }
