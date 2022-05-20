5375b7175458d615db6329af91a3172922cabd0d
Alaeddine Jmila <jmila.aladin@gmail.com>
2022-05-20 17:25:16 +0100

    Added features buttons and header

--- src/UI/UIActions.js
+++ src/UI/UIActions.js
@@ -1,5 +1,6 @@
 export default function UIActions() {
   const dropDownBtns = document.querySelectorAll('.dropdown-btn');
+  const featureBtn = document.querySelectorAll('.feature-btn');
 
   dropDownBtns.forEach(btn => {
     btn.addEventListener('click', function () {
@@ -9,6 +10,12 @@ export default function UIActions() {
       dropdownContent.style.display === 'block'
         ? (dropdownContent.style.display = 'none')
         : (dropdownContent.style.display = 'block');
+    });
+  });
+
+  featureBtn.forEach(btn => {
+    btn.addEventListener('click', function () {
+      this.classList.toggle('active');
     });
   });
 }
